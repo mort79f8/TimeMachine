54cb515c06ad5256efe54330f4e357004a9f131d
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-25 09:01:25 +0200

    Created Solution for TimeMachine. Created 3 project. Class libary, Console Test, Web. Add functionality to class libary Add small test to Console test.

---
+++ TimeMachine.FileLog/FileLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TimeMachine.FileLog
+{
+    public static class FileLogger
+    {
+        private static string url;
+
+        public static string Url { get => url; set => url = value; }
+
+        public static void WriteLine(string value)
+        {
+            if(!File.Exists(Url))
+            {
+                Console.WriteLine("It seems like the url to the file is incorrect");
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(Url, true, Encoding.Default))
+                {
+                    writer.WriteLine(DateTime.Now.ToString("yyyy" + "-" + "mm" +"-" + "dd" + "-" + "HH:MM:SS") + ": " + value);
+                }
+                Console.WriteLine("string add to log");
+            }
+        }
+    }
+}
---
+++ TimeMachine.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeMachine.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeMachine.FileLog.FileLogger.Url = @"D:\test\log.txt";
+            TimeMachine.FileLog.FileLogger.WriteLine("This is a string");
+        }
+    }
+}
